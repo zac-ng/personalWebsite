d185cb9d894a6f1e966bd4dd031bfc9f046a62ac
zac-ng <zac.ng201@gmail.com>
2024-03-02 16:29:07 -0500

    Add fade transitions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,34 @@
 import * as React from "react";
-import { ChakraProvider, Center, Heading, Text, Flex, Button, Icon } from "@chakra-ui/react";
+import { ChakraProvider, Center, Heading, Text, Flex, Button, Icon, SlideFade, Fade } from "@chakra-ui/react";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import Navbar from "./components/Navbar";
 import theme from "./theme";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Navbar />
+    <Fade in transition={{ enter: { delay: 1.5, duration: 1.5 } }}>
+      <Navbar />
+    </Fade>
     <Center minH="100vh" flexDirection="column">
-      <Heading size="2xl" mb={4}>Zachary Nguyen</Heading>
-      <Text fontSize="lg" mb={4}>Student, developer, croissant enthusiast</Text>
-      <Flex alignItems="center">
-        <Button as="a" variant="unstyled" colorScheme="gray" leftIcon={<Icon as={FaGithub} />} mr={6} href="https://github.com/zac-ng" target="_blank">
-          GitHub
-        </Button>
-        <Button as="a" variant="unstyled" colorScheme="gray" leftIcon={<Icon as={FaLinkedin} />} mr={6} href="https://www.linkedin.com/in/zac-ng" target="_blank">
-          LinkedIn
-        </Button>
-        <Button as="a" variant="unstyled" colorScheme="gray" leftIcon={<Icon as={FaEnvelope} />} mr={6} href="mailto:zncodebox@gmail.com">
-          Email
-        </Button>
-      </Flex>
+      <SlideFade in offsetY={30} transition={{ enter: { duration: 2 } }}>
+        <Heading size="2xl" mb={4}>Zachary Nguyen</Heading>
+      </SlideFade>
+      <Fade in transition={{ enter: { delay: 1.5, duration: 1.5 } }}>
+        <Center flexDirection="column">
+          <Text fontSize="lg" mb={4}>Student, developer, croissant enthusiast</Text>
+          <Flex justifyContent="center">
+            <Button as="a" variant="unstyled" colorScheme="gray" leftIcon={<Icon as={FaGithub} />} mr={6} href="https://github.com/zac-ng" target="_blank">
+              GitHub
+            </Button>
+            <Button as="a" variant="unstyled" colorScheme="gray" leftIcon={<Icon as={FaLinkedin} />} mr={6} href="https://www.linkedin.com/in/zac-ng" target="_blank">
+              LinkedIn
+            </Button>
+            <Button as="a" variant="unstyled" colorScheme="gray" leftIcon={<Icon as={FaEnvelope} />} href="mailto:zncodebox@gmail.com">
+              Email
+            </Button>
+          </Flex>
+        </Center>
+      </Fade>
     </Center>
   </ChakraProvider>
 );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <Flex alignItems="center" justifyContent="flex-end" p={4} position="absolute" top="0" left="0" right="0">
       <Flex alignItems="center">
-        <Link mr={4}>Home</Link>
+        <Link mr={4}>About</Link>
         <Link mr={4}>Experience</Link>
         <Link mr={4}>Projects</Link>
         <ColorModeSwitcher ml={4} />
